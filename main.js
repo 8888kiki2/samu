1644fd13ac7f79b41fcd59cc8f648d905c1e7e99
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-03-05 09:04:22 -0300

    p

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 alert('bem vindos a lazyTown');
 let nomeUsuario = prompt('Qual é seu nome??');
 let anterior = document.querySelector('#texto');
+ 
+while(nomeUsuario == ""){
+    nomeUsuario == prompt('Qual é seu nome??');
+}
 
 anterior.textContent = nomeUsuario;
