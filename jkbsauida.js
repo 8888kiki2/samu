0e28341cf2ea850487176f011d30a30f5d8a1ae4
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-03-15 11:37:06 -0300

    dsda

---
+++ jkbsauida.js
@@ -0,0 +1,11 @@
+let numeroSecreto = 8;
+
+let chute = prompt('escolha um numero de 0 a 10');
+
+if(numeroSecreto == chute){
+    alert(párabens voce é um bosta :);
+    console.log(parabens acertou!!!)
+} else{
+    alert('errou, que pena ')
+    console.log('infelizmente, errou!!')
+}
