7cd7577ffbad47ca9c0e60cf1a81928c19842b03
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-04-05 12:14:41 -0300

    sad

--- cont.js
+++ cont.js
@@ -4,7 +4,7 @@ for (let x = 0; x<fodoes.length; x++){
     console.log(x)
    fodoes[x].onclick = function() {
     fodoes[x].classList.add('ativo'); 
-   } fodoes[x].classList.add('ativo'); 
+   } 
 }
 
 
