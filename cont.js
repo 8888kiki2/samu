6af8c86deeef77f8559a05ec447c8b41c92ca4b1
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-04-05 12:20:12 -0300

    asd

--- cont.js
+++ cont.js
@@ -3,8 +3,12 @@ const fodoes = document.querySelectorAll('.objetivos');
 for (let x = 0; x<fodoes.length; x++){
     console.log(x)
    fodoes[x].onclick = function() {
+    for(let y = 0; y< fodoes.length; y++){
+        fodoes[y].classList.remove('ativo')
+    }
     fodoes[x].classList.add('ativo'); 
    } 
+  // fodoes[x].classList.remove('ativo')
 }
 
 
