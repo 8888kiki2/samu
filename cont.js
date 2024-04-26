9b92e7ace52e3cbc7fdffc35264a2b2cb274eff2
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-04-26 09:04:00 -0300

    ndf

--- cont.js
+++ cont.js
@@ -16,7 +16,7 @@ for (let x = 0; x < fodoes.length; x++) {
 const contadores = document.querySelectorAll('.contador');
 
 let tempoAtual = new Date();
-let tempoobjetivo1 = new Date('2024-04-28T00:00:00');
+let tempoObjetivo1 = new Date('2024-04-28T00:00:00');
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 
 let segundos = Math.floor(tempoFinal / 1000);
