51e2b1f706ff24e7e9d2a489c180b0b76d9d9d18
8888kiki2 <159142641+8888kiki2@users.noreply.github.com>
2024-04-26 08:48:58 -0300

    vxc

--- cont.js
+++ cont.js
@@ -10,7 +10,21 @@ for (let x = 0; x<fodoes.length; x++){
    } 
   // fodoes[x].classList.remove('ativo')
 }
+const contadores = document.querySelectorAll('.contador');
 
+let tempoAtual = new Date();
+
+let tempoobjetivo1 = new Date ('2024-04-28T00:00:00');
+
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+
+let segundos = Math.floor(tempoFinal/1000);
+
+let minutos = Math.floor(segundos/60);
+
+let horas = Math.floor(minutos/60);
+
+let dia = Math.floor(horas/24);
 
 
 
